87b367d824f92b64b8db0f47d999e53fecb72189
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2012-07-05 14:51:30 +0200

    Fixed test for Python 2.6

--- djmongo/tests/test_test_case.py
+++ djmongo/tests/test_test_case.py
@@ -6,8 +6,8 @@ class TestOurTestCase(TestCase):
 
     def test_assertDocumentsEqual(self):
 
-        self.assertDocumentsEqual([{'foo': 'bar'}, {'baz', 'tar'}],
-            [{'foo': 'bar'}, {'baz', 'tar'}])
+        self.assertDocumentsEqual([{'foo': 'bar'}, {'baz': 'tar'}],
+            [{'foo': 'bar'}, {'baz': 'tar'}])
 
     def test_assertDocumentsEqual_ommit_mongo_id(self):
 
@@ -18,23 +18,23 @@ class TestOurTestCase(TestCase):
     def test_assertDocumentsEqual_works_for_documents(self):
 
         self.assertDocumentsEqual([Document(data={'foo': 'bar'}),
-            Document(data={'baz', 'tar'})],
-            [{'foo': 'bar'}, {'baz', 'tar'}])
+            Document(data={'baz': 'tar'})],
+            [{'foo': 'bar'}, {'baz': 'tar'}])
 
     def test_assertDocumentsEqual_works_for_iterators(self):
 
-        self.assertDocumentsEqual(iter([{'foo': 'bar'}, {'baz', 'tar'}]),
-            iter([{'foo': 'bar'}, {'baz', 'tar'}]))
+        self.assertDocumentsEqual(iter([{'foo': 'bar'}, {'baz': 'tar'}]),
+            iter([{'foo': 'bar'}, {'baz': 'tar'}]))
 
     def test_assertDocumentsEqual_raises(self):
 
         with self.assertRaises(AssertionError):
-            self.assertDocumentsEqual([{'foo': 'bar'}, {'baz', 'tar'}],
-                [{'foo': 'bar'}, {'foo', 'bar'}])
+            self.assertDocumentsEqual([{'foo': 'bar'}, {'baz': 'tar'}],
+                [{'foo': 'bar'}, {'foo': 'bar'}])
 
     def test_assertDocumentsEqual_raises_for_different_number_of_items(self):
 
         with self.assertRaises(AssertionError):
             self.assertDocumentsEqual([{'foo': 'bar'}],
-                [{'foo': 'bar'}, {'foo', 'bar'}])
+                [{'foo': 'bar'}, {'foo': 'bar'}])
 
