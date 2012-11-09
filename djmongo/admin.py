8e9424cfb2b39c62f6820e69bd977b993cdbb802
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2012-11-09 14:04:54 +0100

    Extended admin support

--- djmongo/admin.py
+++ djmongo/admin.py
@@ -1,7 +1,48 @@
+"""
+This module allows Django users to show djmongo.documents.Document within
+admin site.
+
+Example::
+
+    from django.contrib import admin
+    from djmongo.admin import DocumentAdmin
+    from myapp.documents import Item
+
+
+    class ItemAdmin(DocumentAdmin):
+        document_list_display = ['title', 'url']
+
+        def url(self, item):
+            url = item.data.get('url', None)
+            if not url:
+                return '-'
+            return '<a href="{url}">{url}</a>'.format(url=url)
+        url.allow_tags = True
+
+    admin.site.register(fix_document_for_admin(Item), SouthParkAdmin)
+
+
+
+Admin support should be considered as work-in-progress.
+"""
 from django.contrib import admin
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
 import re
+
+
+DOCUMENT_PROXY_VAR_NAME = 'document_proxy'
+
+
+def fix_document_for_admin(document):
+    opts = document._meta
+    opts.abstract = False
+    opts.get_add_permission = lambda : 'add_%s' % opts.object_name.lower()
+    opts.get_change_permission = lambda : 'change_%s' % opts.object_name.lower()
+    opts.get_delete_permission = lambda : 'delete_%s' % opts.object_name.lower()
+    return [document]
 
 
 class DocumentChangeList(object):
@@ -35,12 +76,40 @@ class DocumentChangeList(object):
         return self.paginator.page(self.page_num + 1).object_list
 
 
+class DocumentProxy(object):
+    """
+    Intermediate class for easier extraction of required data.
+    """
+    def __init__(self, document_admin):
+        """
+        :param document_admin: DocumentAdmin instance
+        """
+        self.document_admin = document_admin
+
+    def get(self, item, key):
+        """
+        :param item: Document instance
+        :param key: string
+        """
+        method = getattr(self.document_admin, key, None)
+        if method:
+            val = method(item)
+            if getattr(method, 'allow_tags', False):
+                val = mark_safe(val)
+            else:
+                val = escape(val)
+            return val
+        return item.data.get(key)
+
 
 class DocumentAdmin(admin.ModelAdmin):
     fields = []
     document_list_display = ['_id']
     search_field = 'title'
     changelist_cls = DocumentChangeList
+
+    def get_document_proxy(self):
+        return DocumentProxy(self)
 
     @admin.options.csrf_protect_m
     def changelist_view(self, request, extra_context=None):
@@ -55,6 +124,7 @@ class DocumentAdmin(admin.ModelAdmin):
 
         data = {
             'items': changelist.get_results(),
+            DOCUMENT_PROXY_VAR_NAME: self.get_document_proxy(),
             'is_popup': False,
             'opts': self.model._meta,
             'cl': changelist,
--- djmongo/templatetags/djmongo_tags.py
+++ djmongo/templatetags/djmongo_tags.py
@@ -1,11 +1,13 @@
 from django import template
+from djmongo.admin import DOCUMENT_PROXY_VAR_NAME
 
 register = template.Library()
 
 
-@register.simple_tag
-def get(data, key):
-    val = data.get(key)
+@register.simple_tag(takes_context=True)
+def get(context, item, key):
+    proxy = context[DOCUMENT_PROXY_VAR_NAME] # Instance of djmongo.admin.DocumentProxy
+    val = proxy.get(item, key)
     if val is None:
         return '-'
     return val
