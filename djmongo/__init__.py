b68fa9b13fc2cddb315baaa71749bd9c5c6a66bc
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2012-11-30 14:12:33 +0100

    Bumped version

--- djmongo/__init__.py
+++ djmongo/__init__.py
@@ -1,7 +1,7 @@
 """
 Implementation of per object permissions for Django 1.2 or later.
 """
-VERSION = (0, 1, 8)
+VERSION = (0, 1, 9, 'dev')
 
 __version__ = '.'.join((str(each) for each in VERSION[:4]))
 
